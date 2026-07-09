21440e4261332798808b1f485b94ec408a370c61
saayi1998 <ananthasaayisaayi@gmail.com>
2026-07-09 10:12:24 +0530

    Grade Calculator

--- Ternary Operator.java
+++
@@ -1,9 +0,0 @@
-class TernaryOperator{
-	public static void main (String args[]){
-		int marks = 85;
-		String result;
-		
-		result=(marks>=50)?"PASS" :"FAIL";
-		System.out.println(result);
-	}
-}
--- variable1.java
+++
@@ -1,7 +0,0 @@
-class variable1 {
-	public static void main(String args[]){
-		int x;      //decleration 
-		x = 10;     // intialization 
-	System.out.println("x:" + x);
-	}
-}
