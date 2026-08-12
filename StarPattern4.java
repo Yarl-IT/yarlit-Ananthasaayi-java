34c11d1c9bd7a1a7b001cd88a0a0a88f576960f0
saayi1998 <ananthasaayisaayi@gmail.com>
2026-08-12 10:55:49 +0530

    Create StarPattern4.java

---
+++ StarPattern4.java
@@ -0,0 +1,16 @@
+class StarPattern4{
+	public static void main(String args[]){
+		int n=5;
+	
+		for(int rows=1;rows<=n;rows++){ 
+			for(int cols1=rows;cols1<=n;cols1++){
+				System.out.print(" ");
+			}	
+			for(int cols2=1;cols2<=2*rows-1;cols2++){
+				System.out.print("*");
+			}	
+		System.out.println();
+		}
+	}
+}
+	
